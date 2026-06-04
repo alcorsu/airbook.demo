d29f6c10c83862ef4990729074e1a7e980357d27
alcorsu <alcorsu@gmail.com>
2026-06-04 19:25:12 -0400

    Actualizar WEBAPP_URL AIRBOOK

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.AIRBOOK_CONFIG = {
-  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxBp8tcp8H3A4OyZKyhNwhv38FhjxLA-If6mJzznE0zcQJr3wzx9i2sDeTZY-Nx1MCQ/exec"
+  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbwqSXX_MFtk3bji13eeqJ2H5LbvP9aQDqOyAV2TA343DAqdj7BmYS6fAb2shUZNctkT/exec"
 };
